d208ed8aa5f04ef3649f864ff96e08a738908820
Cesare Ferrari <cesare@maryandferrari.com>
2021-03-24 10:02:23 -0400

    Implement accordion

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  return (
+    <div>
+      <article className="question">
+        <header>
+          <h4>{title}</h4>
+          <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+            {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          </button>
+        </header>
+      </article>
+
+      {showInfo && <p>{info}</p>}
+    </div>
+  );
 };
 
 export default Question;
